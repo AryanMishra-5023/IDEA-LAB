35a0514287d120fca55897b02bca07f21e5ab762
Mishra Aryan Brijesh <hs.mishraaryan2005@gmail.com>
2025-09-29 12:34:30 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class calc{
+public:
+
+}
+int main(){
+int a,b;
+cout << "Enter two numbers: " << endl;
+cin >> a >> b;
+cout << "Addition is " << a+b << endl;
+cout << "Subtraction is " << a-b << endl;
+cout << "Multiplication is " << a*b << endl;
+cout << "Division is " << a/b << endl;
+
+}
