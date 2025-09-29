09da27334ba5091ded860102e0bb495d41715900
Mishra Aryan Brijesh <hs.mishraaryan2005@gmail.com>
2025-09-29 12:28:02 +0530

    Add files via upload

---
+++ age_eligibility.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int age;
+printf("Enter the age: ");
+scanf("%d",&age);
+if (age>=18)
+ printf("you are eligible for vote");
+else
+ printf("You are not eligible for vote");
+return 0;
+}
